db5ddb1a59f3ff967aa5135a5b2ae0c05584e6fa
Tuomas Rislakki <tuomas2448@gmail.com>
2024-02-27 16:35:13 +0200

    First communication with git

--- src/main.py
+++ src/main.py
@@ -2,13 +2,29 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import asyncio
+import httpx
 
 app = FastAPI()
+
+
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+
+async def request(client, URL):
+    response = await client.get(URL)
+    return response.text
+
+async def task(URL):
+    async with httpx.AsyncClient() as client:
+        result = await request(client, URL)
+        #result = await asyncio.gather(*tasks)
+        return result
+
 
 @app.get("/")
 async def read_root():
@@ -22,3 +38,9 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/git/{username}")
+async def get_git(username: str):
+    URL = f"https://api.github.com/users/{username}/starred"
+    return await task(URL)
+
