216844e410f55eef230cdf0be87d484d0daa6993
Tuomas Rislakki <tuomas2448@gmail.com>
2024-02-29 21:45:00 +0200

    Finished functionality

--- src/main.py
+++ src/main.py
@@ -28,11 +28,23 @@ async def request(client, URL):
     response = await client.get(URL, headers=headers)
     return response.text
 
-def process_data(data):
-    wanted_data = []
+async def process_data(data):
+    repos = []
     for item in data:
         if item["private"] == False:
-            wanted_data.append(item)
+            modified_item = {
+                "name" : item["name"],
+                "description" : item["description"],
+                "url" : item["url"],
+                "topics" : item["topics"]
+            }
+            if item["license"] != None:
+                modified_item["license"] = item["license"]
+            repos.append(modified_item)
+    wanted_data = {
+        "amount" : len(repos),
+        "repositories" : repos
+    }
     return wanted_data
 
         
@@ -76,7 +88,8 @@ async def git_auth(code: str, settings: Annotated[Settings, Depends(get_settings
         #return access_token
         data = await get_data(access_token)
         data = json.loads(data)
-        return process_data(data)
+        wanted_data = await process_data(data)
+        return wanted_data
 
     
 @app.get("/git/auth/{username}")
