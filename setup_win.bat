e56132437a67425c7674a43d17a49154360a988b
Turmarr <tuomas2448@gmail.com>
2024-02-29 10:12:56 +0200

    FUnctionality almost finished, with bat files to help with setup and running

---
+++ setup_win.bat
@@ -0,0 +1,5 @@
+pip install fastapi
+pip install httpx
+pip install "uvicorn[standard]"
+
+copy .\secret\client-empty.py .\secret\client.py
--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 import asyncio
 import httpx
+import json
 
 app = FastAPI()
 
+import secret.client
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def request(client, URL):
     response = await client.get(URL)
@@ -24,23 +23,47 @@ async def task(URL):
         result = await request(client, URL)
         #result = await asyncio.gather(*tasks)
         return result
+    
+async def get_data(acces_token):
+    URL = f"https://api.github.com/user/starred"
+    headers = {
+        'X-GitHub-Api-Version': '2022-11-28',
+        "Authorization" : f"Bearer {acces_token["access_token"]}"
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL, headers=headers)
+        return response.text
 
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/git/{username}")
+@app.get("/git/noauth/{username}")
 async def get_git(username: str):
     URL = f"https://api.github.com/users/{username}/starred"
     return await task(URL)
 
+@app.get("/git/auth/")
+async def git_auth(code: str):
+    URL = f"https://github.com/login/oauth/access_token"
+    params = {
+        "client_id" : secret.client.ID, 
+        "client_secret" : secret.client.SECRET,
+        "code" : code
+    }
+    headers = {
+        "Accept": "application/json"
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(URL, params=params, headers=headers)
+        access_token = json.loads(response.text)
+        #return access_token
+        return await get_data(access_token)
+    
+
+    
+@app.get("/git/auth/{username}")
+async def git_login(username: str):
+    #?login={username}&client_id={secret.client.ID}
+    URL = f"https://github.com/login/oauth/authorize?login={username}&client_id={secret.client.ID}"
+    params = {
+        "login" : username,
+        "client_id" : secret.client.ID
+    }
+    return RedirectResponse(URL)
